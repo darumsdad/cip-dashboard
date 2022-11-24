002194c95953bc0cebc11e4f0e6f1997e7043982
darumsdad <darumsdad@gmail.com>
2022-11-24 04:33:20 -0500

    ss

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/cip-dashboard'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/cip-dashboard/index.html'));
 });
